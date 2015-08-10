0c6df45f5e51fbaff859b3b1149ca3918e4d4343
HenryLee <henrylee_cn@foxmail.com>
2015-08-10 14:14:10 +0800

    精简函数

--- util/util.go
+++ util/util.go
@@ -3,19 +3,7 @@ package util
 
 import (
 	"net/url"
-	"os"
 )
-
-// FileExists returns if the given file exists.
-func FileExists(file string) bool {
-	if _, err := os.Stat(file); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-
-	return true
-}
 
 // 返回编码后的url.URL指针、及解析错误
 func UrlEncode(urlStr string) (*url.URL, error) {
