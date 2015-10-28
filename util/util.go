f54c7a7f77c429e0f190b0c7e6162c71bd8509dc
HenryLee <henrylee_cn@foxmail.com>
2015-10-28 16:39:33 +0800

    增加hash方法

--- util/util.go
+++ util/util.go
@@ -2,6 +2,8 @@
 package util
 
 import (
+	"fmt"
+	"hash/crc32"
 	"net/url"
 )
 
@@ -10,4 +12,12 @@ func UrlEncode(urlStr string) (*url.URL, error) {
 	urlObj, err := url.Parse(urlStr)
 	urlObj.RawQuery = urlObj.Query().Encode()
 	return urlObj, err
+}
+
+// 制作特征值
+func MakeHash(s string) string {
+	const IEEE = 0xedb88320
+	var IEEETable = crc32.MakeTable(IEEE)
+	hash := fmt.Sprintf("%x", crc32.Checksum([]byte(s), IEEETable))
+	return hash
 }
